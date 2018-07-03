f7a171ea17f7dcf77bc4dca3d4eca76c2d65533e
kseewer <kevin.seewer@caramail.com>
2018-07-03 16:03:02 +0200

    Ajout de la barre de menu et recherche

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <template>
-  <div id="app">
-    
+  <div id="app">  
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Anime <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Manga</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Favoris</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" placeholder="Search" type="text">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
+    </form>
+  </div>
+
+</nav>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
