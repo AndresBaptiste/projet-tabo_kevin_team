8e285821a3cab27fa99ef4e94b8ff83637eafb73
TabzKid <kidane.tabo@gmail.com>
2018-07-04 23:11:02 +0200

    Modification App.vue pour mise en cache de données Type "genre" et "catégorie"

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
 
 
 </nav>
-    <router-view/>
+    <router-view v-on:getGenres="cacheGenre" v-on:getCategories="cacheCategorie"/>
   </div>
 </template>
 
@@ -56,5 +56,32 @@
 }
 </style>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      listGenres: [],
+      listCategorie: []
+    };
+  },
+  methods: {
+    cacheGenre: function(dataBrut) {
+      // Mapping sous un objet genre plus light pour la mise en cache
+      this.listGenres = dataBrut.map(e => {
+        var genre = {};
+        genre.id = e.id;
+        genre.name = e.attributes.name;
+        return genre;
+      });
+    },
+    cacheCategorie: function(dataBrut) {
+      // Mapping sous un objet categorie plus light pour la mise en cache
+      this.listCategories = dataBrut.map(e => {
+        var categorie = {};
+        categorie.id = e.id;
+        categorie.name = e.attributes.title;
+        return categorie;
+      });
+    }
+  }
+};
 </script>
