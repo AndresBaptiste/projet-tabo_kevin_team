b2e4fc743a62a0c56571aae2c5542c97b58a4e51
TabzKid <kidane.tabo@gmail.com>
2018-07-04 08:35:33 +0200

    Test API axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="home">
+
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+axios
+  .get("https://kitsu.io/api/edge/genres?page[limit]=10&page[offset]=0")
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
 export default {
   name: "home"
 };
