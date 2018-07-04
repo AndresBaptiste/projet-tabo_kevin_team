405e7d23c88ccc797c04ef922d82504d154c4ef1
TabzKid <kidane.tabo@gmail.com>
2018-07-04 15:13:57 +0200

    Modification du Top 10 Intégration du composant Card.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,20 @@
 <template>
   <div class="home">
-	<div class="jumbotron">
 		<h1 class="display-3">Top 10</h1>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4" v-for="anime in listAnimeTop10" v-bind:key="anime.id">
-           <div class="card mb-4" style="width: 18rem;">
-          <img class="card-img-top" v-bind:src="getImage(anime.posterImage)" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">{{ anime.name }}</h5>
-            <p class="card-text">{{ anime.synopsis.slice(0, 200) }}...</p>
-            <a href="#" class="btn btn-primary">Favoris</a>
-          </div>
-        </div> 
+           <anime-card v-bind:anime="anime" v-bind:isFavoris="false" v-on:actionToBind="ajoutFavoris(anime)">
+           </anime-card> 
 				</div>
 			</div>
-		</div>
 	</div>
 </div>
-
-
 </template>
 
 <script>
 import axios from "axios";
+import Card from "@/components/Card.vue";
 //import anime from "../anime.js";
 
 export default {
@@ -31,7 +22,8 @@ export default {
   data() {
     return {
       name: "",
-      listAnimeTop10: []
+      listAnimeTop10: [],
+      listAnimeFavoris: []
     };
   },
   mounted() {
@@ -40,9 +32,12 @@ export default {
   methods: {
     getTop10() {
       axios
-        .get("https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=0")
+        .get(
+          "https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=0&sort=popularityRank&include=genres"
+        )
         .then(response => {
           var listAnime = response.data.data;
+          //var listGenre = response.data.included;
 
           for (var i = 0; i < listAnime.length; i++) {
             let myAnime = {};
@@ -51,18 +46,23 @@ export default {
             myAnime.synopsis = listAnime[i].attributes.synopsis;
             myAnime.posterImage = listAnime[i].attributes.posterImage.small;
             myAnime.genres = listAnime[i].attributes.genres;
+            myAnime.popularityRank = listAnime[i].attributes.popularityRank;
 
             this.listAnimeTop10.push(myAnime);
           }
-          console.log(response);
+          console.log(response.data);
         })
         .catch(function(error) {
           console.log(error);
         });
     },
-    getImage(url) {
-      return url;
+    ajoutFavoris(anime) {
+      this.listAnimeFavoris.push(anime);
+      console.log(this.listAnimeFavoris);
     }
+  },
+  components: {
+    "anime-card": Card
   }
 };
 </script>
