384aeb49389e385ab995e703857b676429e72ff2
TabzKid <kidane.tabo@gmail.com>
2018-07-04 23:24:23 +0200

    Refactoring du code Manga.vue

--- src/views/Manga.vue
+++ src/views/Manga.vue
@@ -3,8 +3,8 @@
 		<h1 class="display-3">Top Manga</h1>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4" v-for="anime in listMangaTop10" v-bind:key="anime.id">
-           <anime-card v-bind:anime="anime" v-bind:isFavoris="isFavoris(anime)" v-on:add="ajoutFavoris(anime)" v-on:remove="removeFavoris(anime)">
+				<div class="col-md-4" v-for="manga in listMangaTop10" v-bind:key="manga.id">
+           <anime-card v-bind:anime="manga" v-bind:isFavoris="isFavoris(manga)" v-on:add="ajoutFavoris(manga)" v-on:remove="removeFavoris(manga)">
            </anime-card> 
 				</div>
 			</div>
@@ -15,7 +15,7 @@
 <script>
 import axios from "axios";
 import Card from "@/components/Card.vue";
-const FAVORIS = "favoris";
+const FAVORIS = "favorisManga";
 
 export default {
   name: "home",
@@ -56,28 +56,28 @@ export default {
           console.log(error);
         });
     },
-    ajoutFavoris(anime) {
-      const index = this.listMangaFavoris.indexOf(anime);
+    ajoutFavoris(manga) {
+      const index = this.listMangaFavoris.indexOf(manga);
       if (index === -1) {
-        this.listMangaFavoris.push(anime);
+        this.listMangaFavoris.push(manga);
         this.saveToLocalStorage();
       }
       console.log(this.listMangaFavoris);
     },
-    removeFavoris(anime) {
-      const index = this.listMangaFavoris.map(e => e.id).indexOf(anime.id);
+    removeFavoris(manga) {
+      const index = this.listMangaFavoris.map(e => e.id).indexOf(manga.id);
       if (index !== -1) {
         this.listMangaFavoris.splice(index, 1);
         this.saveToLocalStorage();
       }
       console.log(this.listMangaFavoris);
     },
-    isFavoris(anime) {
-      if (this.listMangaFavoris.some(e => e.id === anime.id)) {
+    isFavoris(manga) {
+      if (this.listMangaFavoris.some(e => e.id === manga.id)) {
         return true;
       }
       return false;
-      //return this.listMangaFavoris.includes(anime);
+      //return this.listMangaFavoris.includes(manga);
     },
     saveToLocalStorage() {
       localStorage.setItem(FAVORIS, JSON.stringify(this.listMangaFavoris));
