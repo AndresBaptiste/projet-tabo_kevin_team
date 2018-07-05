cfe28e5c9163db0519e31fd986970870e16a9913
kseewer <kevin.seewer@caramail.com>
2018-07-05 19:12:06 +0200

    modification de la mise en page

--- src/components/GridCard.vue
+++ src/components/GridCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="gridcard">
         <div class="row">
-            <div v-if="loading" class="col-lg-12" style="font-size:24px;"><strong>Loading...</strong></div>
+            <div v-if="loading" class="col-lg-12" style="font-size:24px;"><div class=typewriter><h5>Loading...</h5></div></div>
             <div v-else class="col-md-4" v-for="media in listMedia" v-bind:key="media.id">
                 <media-card v-bind:media="media">
                 </media-card>
