416bc45add2ac5c8c1105863643c6109c52dec8c
TabzKid <kidane.tabo@gmail.com>
2018-07-05 18:48:49 +0200

    Modification de la gestion de favoris. Les listes des favoris sont stockées au niveau $root.

--- src/main.js
+++ src/main.js
@@ -7,8 +7,48 @@ import "bootswatch/dist/spacelab/bootstrap.min.css";
 import "./main.css";
 
 Vue.config.productionTip = false;
+const FAVORIS_MANGA = "favorisManga";
+const FAVORIS_ANIME = "favorisAnime";
 
 new Vue({
+  data() {
+    return {
+      TYPE_MEDIA: {
+        ANIME: 1,
+        MANGA: 2
+      },
+      listAnimeFavoris: JSON.parse(localStorage.getItem(FAVORIS_ANIME)) || [],
+      listMangaFavoris: JSON.parse(localStorage.getItem(FAVORIS_MANGA)) || []
+    };
+  },
+  methods: {
+    saveToLocalStorage(typeMedia) {
+      if (typeMedia === this.TYPE_MEDIA.ANIME) {
+        localStorage.setItem(
+          FAVORIS_ANIME,
+          JSON.stringify(this.listAnimeFavoris)
+        );
+      } else if (typeMedia === this.TYPE_MEDIA.MANGA) {
+        localStorage.setItem(
+          FAVORIS_MANGA,
+          JSON.stringify(this.listMangaFavoris)
+        );
+      }
+    },
+    isFavoris(media) {
+      if (media.typeMedia === this.TYPE_MEDIA.ANIME) {
+        if (this.listAnimeFavoris.some(e => e.id === media.id)) {
+          return true;
+        }
+        return false;
+      } else if (media.typeMedia === this.TYPE_MEDIA.MANGA) {
+        if (this.listMangaFavoris.some(e => e.id === media.id)) {
+          return true;
+        }
+        return false;
+      }
+    }
+  },
   router,
   render: h => h(App)
 }).$mount("#app");
